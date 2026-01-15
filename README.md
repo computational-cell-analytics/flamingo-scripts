43cc0fb9eee52842e5d286e433d4c444e6253fa7
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2026-01-15 21:16:22 +0100

    Add mastodon export script

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Flamingo Scripts
 
-Scripts for flamingo data analysis.
+Scripts for flamingo data analysis:
+- `run_measurement.py`: Run volume measurement for a 3d segmentation.
+    - Use as follows: `python run_measurement.py path-to-vol.tif output.xlsx --voxel_size Z Y X`
+    - It will save the volume for each segment in `path-to-vol.tif` in the output table `output.xlsx` (alternatively can also store a csv file if you pass the ending `.csv`).
+    - `--voxel_size` specifies the size of the voxels (in μm), in order ZYX.
+- `export_for_mastodon.py`: Export a time series of segmentations stored in a folder individual tif files for mastodon.
+    - Use as follows: `python export_for_mastodon.py input_folder output_file.tif --voxel_size Z Y X`
---
+++ export_for_mastodon.py
@@ -0,0 +1,39 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+import tifffile as tif
+
+
+def export_to_mastodon(input_folder, output_tif, voxel_size, unit, dtype=np.uint16):
+    files = sorted(Path(input_folder).glob("*.tif"))
+    if not files:
+        files = sorted(Path(input_folder).glob("*.tiff"))
+    if not files:
+        raise RuntimeError(f"No tif files found in {input_folder}")
+
+    volumes = [tif.imread(f).astype(dtype, copy=False) for f in files]
+    data = np.stack(volumes, axis=0)  # (T, Z, Y, X)
+
+    vz, vy, vx = voxel_size
+    tif.imwrite(
+        output_tif,
+        data,
+        imagej=True,
+        resolution=(1 / vx, 1 / vy),
+        metadata={"axes": "TZYX", "spacing": vz, "unit": "micron"},
+    )
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_folder")
+    parser.add_argument("output_path")
+    parser.add_argument("--voxel_size", nargs=3, type=float, required=True)
+    parser.add_argument("--unit", default="micron")
+    args = parser.parse_args()
+    export_to_mastodon(args.input_folder, args.output_path, args.voxel_size, args.unit)
+
+
+if __name__ == "__main__":
+    main()
